4194898ed7dde05c488c7eb009d90bc93e33cf9f
Nell Hardcastle <nell@dev-nell.com>
2014-06-03 22:04:33 -0700

    Rename coin.conf -> tacocoin.conf.

--- src/util.cpp
+++ src/util.cpp
@@ -1093,7 +1093,7 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
 
 boost::filesystem::path GetConfigFile()
 {
-    boost::filesystem::path pathConfigFile(GetArg("-conf", "coin.conf"));
+    boost::filesystem::path pathConfigFile(GetArg("-conf", "tacocoin.conf"));
     if (!pathConfigFile.is_complete()) pathConfigFile = GetDataDir(false) / pathConfigFile;
     return pathConfigFile;
 }
