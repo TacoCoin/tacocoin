1a50eb833a6c897a986eb75b43933df86957d51f
Nell Hardcastle <nell@dev-nell.com>
2014-06-04 15:26:36 -0700

    Add getdogecoin.com DNS seed.

--- src/net.cpp
+++ src/net.cpp
@@ -1192,7 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {NULL, NULL},
+    {"getdogecoin.com", "seed.getdogecoin.com"},
     {NULL, NULL}
 };
 
