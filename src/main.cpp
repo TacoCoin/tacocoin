a64b22b6b3d22dd07724fd1c54166cfadeae9731
Whit J <whitj00@users.noreply.github.com>
2014-06-10 17:49:45 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3135,7 +3135,7 @@ bool static AlreadyHave(const CInv& inv)
 // The message start string is designed to be unlikely to occur in normal data.
 // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
 // a large 4-byte int at any alignment.
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb }; // Litecoin: increase each by adding 2 to bitcoin's value.
+unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
 
 
 void static ProcessGetData(CNode* pfrom)
