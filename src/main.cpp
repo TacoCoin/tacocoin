323cc56b92e522a0872b89a3afe83ae96e91c6d7
Whit J <whitj00@users.noreply.github.com>
2014-07-17 20:52:22 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1079,13 +1079,9 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     {
         nSubsidy >>= (nHeight / 50000);
     }
-    else if(nHeight < 17)
-    {
-        nSubsidy = 10000 * COIN;
-    }
     else
     {
-        nSubsidy = 1000000 * COIN;
+        nSubsidy = 500000 * COIN;
     }
 
     return nSubsidy + nFees;
