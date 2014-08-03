650539f777adde8859abbbdd70be552a440b8598
Whit J <whitj00@users.noreply.github.com>
2014-08-03 13:49:16 -0700

    Update netbase_tests.cpp

--- src/test/netbase_tests.cpp
+++ src/test/netbase_tests.cpp
@@ -52,20 +52,20 @@ bool static TestSplitHost(string test, string host, int port)
 
 BOOST_AUTO_TEST_CASE(netbase_splithost)
 {
-    BOOST_CHECK(TestSplitHost("www.bitcoin.org", "www.bitcoin.org", -1));
-    BOOST_CHECK(TestSplitHost("[www.bitcoin.org]", "www.bitcoin.org", -1));
-    BOOST_CHECK(TestSplitHost("www.bitcoin.org:80", "www.bitcoin.org", 80));
-    BOOST_CHECK(TestSplitHost("[www.bitcoin.org]:80", "www.bitcoin.org", 80));
+    BOOST_CHECK(TestSplitHost("www.tacocoin.net", "www.tacocoin.net", -1));
+    BOOST_CHECK(TestSplitHost("[www.tacocoin.net]", "www.tacocoin.net", -1));
+    BOOST_CHECK(TestSplitHost("www.tacocoin.net:80", "www.tacocoin.net", 80));
+    BOOST_CHECK(TestSplitHost("[www.tacocoin.net]:80", "www.tacocoin.net", 80));
     BOOST_CHECK(TestSplitHost("127.0.0.1", "127.0.0.1", -1));
-    BOOST_CHECK(TestSplitHost("127.0.0.1:9333", "127.0.0.1", 9333));
+    BOOST_CHECK(TestSplitHost("127.0.0.1:11042", "127.0.0.1", 11042));
     BOOST_CHECK(TestSplitHost("[127.0.0.1]", "127.0.0.1", -1));
-    BOOST_CHECK(TestSplitHost("[127.0.0.1]:9333", "127.0.0.1", 9333));
+    BOOST_CHECK(TestSplitHost("[127.0.0.1]:11042", "127.0.0.1", 11042));
     BOOST_CHECK(TestSplitHost("::ffff:127.0.0.1", "::ffff:127.0.0.1", -1));
-    BOOST_CHECK(TestSplitHost("[::ffff:127.0.0.1]:9333", "::ffff:127.0.0.1", 9333));
-    BOOST_CHECK(TestSplitHost("[::]:9333", "::", 9333));
-    BOOST_CHECK(TestSplitHost("::9333", "::9333", -1));
-    BOOST_CHECK(TestSplitHost(":9333", "", 9333));
-    BOOST_CHECK(TestSplitHost("[]:9333", "", 9333));
+    BOOST_CHECK(TestSplitHost("[::ffff:127.0.0.1]:11042", "::ffff:127.0.0.1", 11042));
+    BOOST_CHECK(TestSplitHost("[::]:11042", "::", 11042));
+    BOOST_CHECK(TestSplitHost("::11042", "::11042", -1));
+    BOOST_CHECK(TestSplitHost(":11042", "", 11042));
+    BOOST_CHECK(TestSplitHost("[]:11042", "", 11042));
     BOOST_CHECK(TestSplitHost("", "", -1));
 }
 
@@ -80,10 +80,10 @@ bool static TestParse(string src, string canon)
 BOOST_AUTO_TEST_CASE(netbase_lookupnumeric)
 {
     BOOST_CHECK(TestParse("127.0.0.1", "127.0.0.1:65535"));
-    BOOST_CHECK(TestParse("127.0.0.1:9333", "127.0.0.1:9333"));
+    BOOST_CHECK(TestParse("127.0.0.1:11042", "127.0.0.1:11042"));
     BOOST_CHECK(TestParse("::ffff:127.0.0.1", "127.0.0.1:65535"));
     BOOST_CHECK(TestParse("::", "[::]:65535"));
-    BOOST_CHECK(TestParse("[::]:9333", "[::]:9333"));
+    BOOST_CHECK(TestParse("[::]:11042", "[::]:11042"));
     BOOST_CHECK(TestParse("[127.0.0.1]", "127.0.0.1:65535"));
     BOOST_CHECK(TestParse(":::", ""));
 }
